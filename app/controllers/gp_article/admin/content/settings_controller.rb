e11c81b068132a999b84e6e7bc4d439c41bc2e0f
mmukai <mukai@sitebridge.co.jp>
2016-02-09 10:36:56 +0900

    Add serial no to GpArticle

--- app/controllers/gp_article/admin/content/settings_controller.rb
+++ app/controllers/gp_article/admin/content/settings_controller.rb
@@ -38,7 +38,7 @@ class GpArticle::Admin::Content::SettingsController < Cms::Controller::Admin::Ba
     if @item.name.in?('gp_category_content_category_type_id', 'calendar_relation', 'map_relation', 'inquiry_setting',
                       'approval_relation', 'gp_template_content_template_id', 'feed', 'tag_relation', 'sns_share_relation',
                       'blog_functions', 'feature_settings', 'list_style', 'qrcode_settings', 'basic_setting',
-                      'rank_relation')
+                      'rank_relation','serial_no_settings')
       extra_values = @item.extra_values
 
       case @item.name
@@ -86,6 +86,8 @@ class GpArticle::Admin::Content::SettingsController < Cms::Controller::Admin::Ba
         extra_values[:state] = params[:state]
       when 'rank_relation'
         extra_values[:rank_content_id] = params[:rank_content_id].to_i
+      when 'serial_no_settings'
+        extra_values[:title] = params[:title]
       end
 
       @item.extra_values = extra_values
--- app/models/gp_article/doc.rb
+++ app/models/gp_article/doc.rb
@@ -84,6 +84,7 @@ class GpArticle::Doc < ActiveRecord::Base
   before_save :set_name
   before_save :set_published_at
   before_save :replace_public
+  before_save :set_serial_no
   before_destroy :keep_edition_relation
   after_destroy :close_page
 
@@ -444,6 +445,7 @@ class GpArticle::Doc < ActiveRecord::Base
       new_doc.published_at = nil
       new_doc.display_published_at = nil
       new_doc.in_tasks = nil
+      new_doc.serial_no = nil
     end
 
     if editable_group
@@ -831,6 +833,12 @@ class GpArticle::Doc < ActiveRecord::Base
 
   def set_published_at
     self.published_at ||= Core.now if self.state == 'public'
+  end
+
+  def set_serial_no
+    return if self.serial_no.present?
+    seq = Util::Sequencer.next_id('gp_article_doc_serial_no', :version => self.content_id)
+    self.serial_no = seq
   end
 
   def set_defaults
