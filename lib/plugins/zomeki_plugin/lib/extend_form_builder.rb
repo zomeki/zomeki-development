ceb062b7668d2e2965b8efb5afec3b6351a7e78f
mmukai <mukai@sitebridge.co.jp>
2016-05-27 18:03:39 +0900

    Fixed a bug in the takeover selected value of the form

--- lib/plugins/zomeki_plugin/lib/extend_form_builder.rb
+++ lib/plugins/zomeki_plugin/lib/extend_form_builder.rb
@@ -17,7 +17,7 @@ class ActionView::Helpers::FormBuilder
       if @template.params[@object_name] && @template.params[@object_name][method]
         return @template.params[@object_name][method]
       else
-        return @template.instance_variable_get("@#{@object_name}").send(method)
+        return (@object || @template.instance_variable_get("@#{@object_name}")).try(method)
       end
     end
     pos = pos.to_i
